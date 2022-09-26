a3ed545a8b20946313b3cd91430d07268d4bcffe
Uchembanefo <110548928+Uchembanefo@users.noreply.github.com>
2022-09-26 23:19:36 +0100

    added celsius and fahreinheit conversion

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,5 +1,6 @@
 function getCityWeather(result) {
   console.log(result);
+  celsiusTemp = `${Math.round(result.data.main.temp)}`;
   let temperature = document.querySelector(".temperature");
   temperature.innerHTML = `${Math.round(result.data.main.temp)}`;
   let humidity = document.querySelector(".humidity");
@@ -80,7 +81,30 @@ function currentWeather(Event) {
   navigator.geolocation.getCurrentPosition(presentPosition);
 }
 
+function changeToFahreinheit(event) {
+  event.preventDefault();
+  let temperature = document.querySelector(".temperature");
+  let fahreinheit = (celsiusTemp * 9) / 5 + 32;
+  temperature.innerHTML = `${Math.round(fahreinheit)}`;
+  celsius.classList.remove("active");
+  fahreinheit.classList.Add("active");
+}
+
+function changeToCelsius(event) {
+  event.preventDefault();
+  let temperature = document.querySelector(".temperature");
+  temperature.innerHTML = `${celsiusTemp}`;
+  celsius.classList.Add("active");
+  fahreinheit.classList.remove("active");
+}
+
 let form = document.querySelector(".cities");
 form.addEventListener("submit", enterCity);
 let currentCity = document.querySelector(".city");
 currentCity.addEventListener("click", currentWeather);
+let celsiusTemp = null;
+let degreeFahreinheit = document.querySelector(".fahreinheit");
+degreeFahreinheit.addEventListener("click", changeToFahreinheit);
+
+let degreeCelsius = document.querySelector(".celsius");
+degreeCelsius.addEventListener("click", changeToCelsius);
