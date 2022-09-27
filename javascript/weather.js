a133326e5763809391ba625cf0dba161d1c1d5ba
Uchembanefo <110548928+Uchembanefo@users.noreply.github.com>
2022-09-27 01:18:20 +0100

    removed current date info from current button

--- javascript/weather.js
+++ javascript/weather.js
@@ -68,27 +68,7 @@ function presentPosition(position) {
 }
 function currentWeather(Event) {
   Event.preventDefault();
-  let now = new Date();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[now.getDay()];
-  let currentDate = document.querySelector(".currentDate");
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  currentDate.innerHTML = `${day} + " " + ${hours} + ":" + ${minutes}`;
+
   navigator.geolocation.getCurrentPosition(presentPosition);
 }
 
